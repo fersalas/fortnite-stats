4cc4a495e4364b70559b5dd4066661accc8f72a0
Fernando Salas <fersalaslopez@gmail.com>
2019-06-14 18:30:26 -0500

    Added Material UI and removed some unused files from CRA

---
+++ src/app/display/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        My Reads
+      </header>
+    </div>
+  );
+}
+
+export default App;
