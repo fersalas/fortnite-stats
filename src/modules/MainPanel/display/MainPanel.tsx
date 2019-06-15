18779a39ea4a73278be84b8e0239eb8b4621836a
Fernando Salas <fersalaslopez@gmail.com>
2019-06-14 20:46:00 -0500

    More MUI-ish content to MainPanel

--- src/modules/MainPanel/display/MainPanel.tsx
+++ src/modules/MainPanel/display/MainPanel.tsx
@@ -1,14 +1,17 @@
 import React, { PureComponent } from 'react'
 import {
-    Paper
+    Paper,
+    Typography
 }                               from '@material-ui/core/';
 
 class MainPanel extends PureComponent {
     render() {
         return (
             <div>
-                <Paper>
-                    {'Hello World!'}
+                <Paper style={{padding: '20px'}}>
+                    <Typography component="h2">
+                        {'Hello World!'}
+                    </Typography>                    
                 </Paper>
             </div>
         )
