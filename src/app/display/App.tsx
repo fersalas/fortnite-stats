1475f9e3be377581a8a78bf77e2d7285236f84ce
Fernando Salas <fersalaslopez@gmail.com>
2019-06-14 21:30:48 -0500

    Setup basic redux saga, also some devtools extension

--- src/app/display/App.tsx
+++ src/app/display/App.tsx
@@ -1,15 +1,21 @@
 import React     from 'react';
+import { Provider as ReduxProvider } from "react-redux";
 import MainNav   from "../../modules/MainNav";
 import MainPanel from "../../modules/MainPanel";
+import getStore, {configureStore}  from '../functional/store';
+
+configureStore();
 
 const App: React.FC = () => {
   return (
-    <div>
-      <header>
-          <MainNav />
-          <MainPanel />
-      </header>
-    </div>
+    <ReduxProvider store={getStore()}>
+      <div>
+        <header>
+            <MainNav />
+            <MainPanel />
+        </header>
+      </div>
+    </ReduxProvider>
   );
 }
 
